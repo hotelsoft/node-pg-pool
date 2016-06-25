4ab39361f2a7876f1003571c4aef7e6979d3a338
Kesav Kumar Kolla <kesav@hotelsoft.com>
2016-06-24 23:32:07 -0700

    Emit event when a connection is obtained from pool

--- test/events.js
+++ test/events.js
@@ -7,34 +7,34 @@ var Pool = require('../')
 
 describe('events', function () {
   it('emits connect before callback', function (done) {
-    var pool = new Pool();
-    var emittedClient = false;
+    var pool = new Pool()
+    var emittedClient = false
     pool.on('connect', function (client) {
-      emittedClient = client;
-    });
+      emittedClient = client
+    })
 
     pool.connect(function (err, client, release) {
-      if (err) return done(err);
-      release();
-      pool.end();
-      expect(client).to.be(emittedClient);
-      done();
-    });
-  });
+      if (err) return done(err)
+      release()
+      pool.end()
+      expect(client).to.be(emittedClient)
+      done()
+    })
+  })
 
   it('emits pool.connect before callback', function (done) {
-    var pool = new Pool();
-    var emittedClient = false;
+    var pool = new Pool()
+    var emittedClient = false
     pool.on('pool.connect', function (client) {
-      emittedClient = client;
-    });
+      emittedClient = client
+    })
 
     pool.connect(function (err, client, release) {
-      if (err) return done(err);
-      release();
-      pool.end();
-      expect(client).to.be(emittedClient);
-      done();
-    });
+      if (err) return done(err)
+      release()
+      pool.end()
+      expect(client).to.be(emittedClient)
+      done()
+    })
   })
-});
+})
